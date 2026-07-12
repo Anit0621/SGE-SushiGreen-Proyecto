27321b8e407433ec35c1131c30b26c7596ddeefb
Camilo <juan.gonzalez.bernal@correounivalle.edu.co>
2026-07-12 16:30:47 -0500

    crud para clientes creado; hace falta Update y verificacion de integridad si cliente tiene una factura asociada

--- run.py
+++ run.py
@@ -1,12 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Blueprint, request, url_for, redirect
 from app.routes.clientes import cliente_bp
+from app.routes.clientes import *
 app = Flask(__name__)
+
 
 app.register_blueprint(cliente_bp, url_prefix='/clientes')
 
-@app.route('/')
+@app.route('/',methods=["GET"])
 def home():
-    return 'Hello World!'
+    return render_template("inicio.html")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
