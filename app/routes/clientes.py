9f4f24e0b87d77e10a4e5b46ed14f1ee66f2cf18
MINITO_JDGV <hulk1234guar@gmail.com>
2026-07-13 13:46:08 -0500

    arreglando error de archivos borrados

---
+++ app/routes/clientes.py
@@ -0,0 +1,55 @@
+from flask import Flask, render_template, Blueprint, request, url_for, redirect
+from app.services.cliente import listar_clientes, filtro_por_doc, crear_cliente_nuevo, todos_clientes, borrado_cliente
+
+
+cliente_bp = Blueprint("cliente", __name__,template_folder="templates")
+
+
+@cliente_bp.route("/<int:limite>")
+def cliente(limite):
+    clientes = listar_clientes(limite)
+    return render_template("clientes.html", clientes=clientes)
+
+@cliente_bp.route("/<string:tipo_doc>")
+def cliente_filtro(tipo_doc):
+    clientes = filtro_por_doc(tipo_doc)
+    return render_template("clientes.html", clientes=clientes)
+
+@cliente_bp.route("/todos")
+def clientes_todos():
+    clientes = todos_clientes()
+    return render_template("clientes.html", clientes=clientes)
+
+@cliente_bp.route("/submit",methods=["POST","GET"])
+def cliente_submit():
+    if request.method == "POST":
+        cedula = request.form["ced"]
+        nombre = request.form["nombre"]
+        apellido = request.form["apellido"]
+        telefono = request.form["telefono"]
+        tipo_documento = request.form["tip_documento"]
+        habeas_data = request.form["habeas_data"]
+        ciudad = request.form["ciudad"]
+        direccion_residencia = request.form["dir_res"]
+        direccion_operativa = request.form["dir_opr"]
+        email = request.form["email"]
+        representante_legal = request.form["repr_legal"]
+        regimen_tributario = request.form["reg_trib"]
+        crear_cliente_nuevo(cedula,nombre,apellido,telefono,tipo_documento,habeas_data,ciudad,direccion_residencia,direccion_operativa,email
+                            ,representante_legal,regimen_tributario)
+        return redirect(url_for("cliente.clientes_todos"))
+    else:
+        return render_template("registrar_cliente.html")
+
+@cliente_bp.route("/delete",methods=["GET","POST"])
+def cliente_delete():
+
+    if request.method == "POST":
+        id_cliente = request.form["ced"]
+        borrado_cliente(id_cliente)
+
+        return redirect(url_for("cliente.clientes_todos"))
+
+    else:
+        return render_template("borrado_cliente.html")
+
